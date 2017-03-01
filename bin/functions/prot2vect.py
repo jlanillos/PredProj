ea3dd4000bdaafdaa4babaf11a7a39660314afa7
jlanillos <javi.lanillos@gmail.com>
2017-03-01 19:45:26 +0100

    Add files via upload

--- bin/functions/prot2vect.py
+++ bin/functions/prot2vect.py
@@ -8,18 +8,18 @@ def prot2vect(seq_list, feature_list):
 #<value> = 0 or 1 (this is valid when you have single sequence)
 
 	from dictionaries import dictionaries
-	aa_dict, feature_dict = dictionaries()
+	aa_dict, feature_dict, aa2vect_dictionary = dictionaries()
 	import numpy as np
 	dic_vect = np.arange(1,len(aa_dict)+1)
 	cont = 0
 	seq2code = list()
 
 
+
 	for seq in seq_list:
-		
-		
+
+
 		for i in seq:
-		
 			aux_vector = np.zeros(len(aa_dict))
 			print(aux_vector)
 			aux_vector[aa_dict[i]-1] = 1 # Put a 1 at the position it corresponds to the aa involved
@@ -32,8 +32,34 @@ def prot2vect(seq_list, feature_list):
 			final_vector[1:] = my_vector
 			cont = cont + 1
 			seq2code.append(final_vector)
-
 	return seq2code
+
+
+		
+#	from dictionaries import dictionaries
+#	aa_dict, feature_dict = dictionaries()
+#	import numpy as np
+#	dic_vect = np.arange(1,len(aa_dict)+1)
+#	cont = 0
+#	seq2code = list()
+
+
+# with 0 1 0 2 0 3 0 4 0 5 0 6 0....20 0
+#	for seq in seq_list:
+#		for i in seq:
+#			aux_vector = np.zeros(len(aa_dict))
+#			print(aux_vector)
+#			aux_vector[aa_dict[i]-1] = 1 # Put a 1 at the position it corresponds to the aa involved
+#			print(aux_vector)
+#			my_vector = np.empty((aux_vector.size + dic_vect.size), dtype=dic_vect.dtype)
+#			my_vector[0::2] = dic_vect
+#			my_vector[1::2] = aux_vector
+#			final_vector = np.zeros(len(my_vector)+1)
+#			final_vector[0] = feature_dict[feature[cont]]
+#			final_vector[1:] = my_vector
+#			cont = cont + 1
+#			seq2code.append(final_vector)
+#	return seq2code
 
 
 		
